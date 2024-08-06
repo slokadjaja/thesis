4341558c996a4e053c67fa71e2cfdf05ea6b503f
inisamuel <sammyloka@yahoo.com>
2024-08-06 23:37:32 +0200

    match cnn kernels to 1611.06455v4

--- model.py
+++ model.py
@@ -42,15 +42,15 @@ class VAE(nn.Module):
             nn.Linear(500, self.input_dim)
         )
 
-        len_after_conv = conv_out_len_multiple(self.input_dim, [(4, 1, 0), (3, 1, 0), (2, 1, 0)])
+        len_after_conv = conv_out_len_multiple(self.input_dim, [(8, 1, 0), (5, 1, 0), (3, 1, 0)])
         cnn_encoder = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=128, kernel_size=4),
+            nn.Conv1d(in_channels=1, out_channels=128, kernel_size=8),
             nn.BatchNorm1d(num_features=128),
             nn.ReLU(),
-            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3),
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5),
             nn.BatchNorm1d(num_features=256),
             nn.ReLU(),
-            nn.Conv1d(in_channels=256, out_channels=128, kernel_size=2),
+            nn.Conv1d(in_channels=256, out_channels=128, kernel_size=3),
             nn.BatchNorm1d(num_features=128),
             nn.ReLU(),
             nn.Flatten(),
@@ -61,14 +61,13 @@ class VAE(nn.Module):
             nn.Flatten(),
             nn.Linear(self.alphabet_size * self.n_latent, len_after_conv * 128),
             nn.Unflatten(dim=1, unflattened_size=(128, len_after_conv)),
-            nn.ConvTranspose1d(in_channels=128, out_channels=256, kernel_size=2),
+            nn.ConvTranspose1d(in_channels=128, out_channels=256, kernel_size=3),
             nn.BatchNorm1d(num_features=256),
             nn.ReLU(),
-            nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=3),
+            nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=5),
             nn.BatchNorm1d(num_features=128),
             nn.ReLU(),
-            nn.ConvTranspose1d(in_channels=128, out_channels=1, kernel_size=4),
-            # nn.ReLU(),
+            nn.ConvTranspose1d(in_channels=128, out_channels=1, kernel_size=8),
         )
 
         if self.model == "fc":
