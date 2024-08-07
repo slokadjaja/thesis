c3b41e68011fa8781e294b9b10896b1cd7e5b827
inisamuel <sammyloka@yahoo.com>
2024-08-07 12:54:06 +0200

    integrate mlflow

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 
 We aim to learn symbolic representations of time series data in an unsupervised manner using 
 variational autoencoders (VAEs) with categorical latent variables + Gumbel-softmax reparametrization
+
+MLFlow is used to log metrics, parameters and artefacts, and the runs are saved in 
+[databricks](https://community.cloud.databricks.com/)
--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from model import VAE
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+import mlflow
 
 if __name__ == "__main__":
     params = Params("params.json")
@@ -39,52 +40,70 @@ if __name__ == "__main__":
     loss_arr, rec_arr, kl_arr = [], [], []
     x, loss, rec_loss, kl_div = None, None, None, None
 
-    # Training loop
-    vae.train()
-    for epoch in tqdm(range(num_epochs), desc="Epoch"):
-        for x, y in train_dataloader:
-            x = x.to(device)
+    mlflow.set_tracking_uri("databricks")
+    exp = mlflow.set_experiment(f'/Users/sammyloka@yahoo.com/{arch}')
 
-            logits, output = vae(x)
-            rec_loss = reconstruction_loss(torch.squeeze(x, dim=1), torch.squeeze(output, dim=1))
-            kl_div = cat_kl_div(logits, n_latent=n_latent, alphabet_size=alphabet_size)
-            loss = rec_loss + beta * kl_div
+    with mlflow.start_run():
+        # Training loop
+        vae.train()
+        for epoch in tqdm(range(num_epochs), desc="Epoch"):
+            for x, y in train_dataloader:
+                x = x.to(device)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                logits, output = vae(x)
+                rec_loss = reconstruction_loss(torch.squeeze(x, dim=1), torch.squeeze(output, dim=1))
+                kl_div = cat_kl_div(logits, n_latent=n_latent, alphabet_size=alphabet_size)
+                loss = rec_loss + beta * kl_div
 
-        loss_arr.append(loss.item())
-        rec_arr.append(rec_loss.item())
-        kl_arr.append(kl_div.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-    vae.eval()
+            loss_arr.append(loss.item())
+            rec_arr.append(rec_loss.item())
+            kl_arr.append(kl_div.item())
 
-    # Encode samples from training set
-    # n = 500
-    # with open('test_patch.npy', 'wb') as f:
-    #     np.save(f, train[:n][0].detach().numpy().squeeze())
-    #     np.save(f, vae.encode(train[:n][0]).detach().numpy().squeeze())
+            mlflow.log_metrics({"total loss": loss.item(),
+                                "reconstruction loss": rec_loss.item(),
+                                "kl divergence": kl_div.item()}, step=epoch
+                               )
 
-    # Plot reconstruction example
-    with torch.no_grad():
-        for i in range(10): #min(batch_size, len(x))):
-            # input shape should be [1, 1, * ] instead of [1, * ], otherwise flatten in cnn_encoder does not work
-            sample = torch.unsqueeze(x[i], 0)
-            plt.plot(sample.squeeze(), label="ground truth")
-            plt.plot(vae(sample)[1].detach().numpy().squeeze(), label="reconstruction")
-            plt.legend()
-            plt.title(f"Reconstruction Example ({dataset})")
-            # plt.savefig(f"recon_patch_{idx}.png", dpi=300)
+        # Log hyperparams, model and model summary
+        mlflow.log_params(params.dict)
+        with open("model_summary.txt", "w") as f:
+            f.write(repr(vae))
+        mlflow.log_artifact("model_summary.txt")
+        mlflow.pytorch.log_model(vae, "model")
+
+        vae.eval()
+
+        # Encode samples from training set
+        # n = 500
+        # with open('test_patch.npy', 'wb') as f:
+        #     np.save(f, train[:n][0].detach().numpy().squeeze())
+        #     np.save(f, vae.encode(train[:n][0]).detach().numpy().squeeze())
+
+        # Plot reconstruction example
+        with torch.no_grad():
+            for i in range(10):  # min(batch_size, len(x))):
+                # input shape should be [1, 1, * ] instead of [1, * ], otherwise flatten in cnn_encoder does not work
+                sample = torch.unsqueeze(x[i], 0)
+                fig, ax = plt.subplots()
+                ax.plot(sample.squeeze(), label="ground truth")
+                ax.plot(vae(sample)[1].detach().numpy().squeeze(), label="reconstruction")
+                ax.legend()
+                plt.title(f"Reconstruction Example ({dataset})")
+                mlflow.log_figure(fig, f'recon_{i}.png')
+                # plt.savefig(f"recon_patch_{idx}.png", dpi=300)
+                plt.show()
+
+            # Plot loss
+            fig, ax = plt.subplots()
+            ax.plot(loss_arr, label="Loss")
+            ax.plot(kl_arr, alpha=0.6, label="KL divergence")
+            ax.plot(rec_arr, alpha=0.6, label="Reconstruction loss")
+            ax.legend()
+            ax.set(xlabel='Epoch', ylabel='Loss')
+            plt.title(f"Loss Curve ({dataset})")
+            # plt.savefig(f"loss_patch.png", dpi=300)
             plt.show()
-
-        # Plot loss
-        fig, ax = plt.subplots()
-        ax.plot(loss_arr, label="Loss")
-        ax.plot(kl_arr, alpha=0.6, label="KL divergence")
-        ax.plot(rec_arr, alpha=0.6, label="Reconstruction loss")
-        ax.legend()
-        ax.set(xlabel='Epoch', ylabel='Loss')
-        plt.title(f"Loss Curve ({dataset})")
-        # plt.savefig(f"loss_patch.png", dpi=300)
-        plt.show()
