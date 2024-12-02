dd8e6a6136bc4219bf59689419a0545264f304ed
inisamuel <sammyloka@yahoo.com>
2024-12-02 16:27:43 +0100

    small changes

--- pipeline.py
+++ pipeline.py
@@ -6,16 +6,22 @@ from tqdm import tqdm
 import itertools
 
 if __name__ == "__main__":
-    datasets = ["Wine", "ArrowHead", "p2s"]
+    # todo set pipeline params externally?
+    train_datasets = ["Wine", "ArrowHead", "p2s"]
     patch_lens = [16, 32, 128]
     alphabet_sizes = [32, 48]
+    use_azure = True
+    hyp_tuning_trials = 3
+    cls_datasets = ["Wine", "Rock", "Plane", "ArrowHead", "p2s", "FordA", "FordB"]
+    sax_params = [{"n_segments": 128, "alphabet_size": 32}, {"n_segments": 128, "alphabet_size": 48}]
+    cls_trials = 10
 
     params = Params("params.json")
     run_names = []
 
-    # todo also test arch
-    for dataset, patch_len, alphabet_size in tqdm(itertools.product(datasets, patch_lens, alphabet_sizes),
-                                                  total=len(datasets) * len(patch_lens) * len(alphabet_sizes),
+    # todo also test arch, triplet loss
+    for dataset, patch_len, alphabet_size in tqdm(itertools.product(train_datasets, patch_lens, alphabet_sizes),
+                                                  total=len(train_datasets) * len(patch_lens) * len(alphabet_sizes),
                                                   desc="Pipeline progress"):
         run_name = f"{dataset}_p{patch_len}_a{alphabet_size}"
         print(f"Starting run: {run_name}")
@@ -27,16 +33,11 @@ if __name__ == "__main__":
         params.alphabet_size = alphabet_size
 
         print("Start tuning hyperparameters")
-        params_tuned = tune_hyperparameters(params, run_name=run_name)  # Hyperparameter tuning
+        params_tuned = tune_hyperparameters(params, run_name=run_name, azure=use_azure, n_trials=hyp_tuning_trials)
 
         print("Hyperparameters tuned, start training using best hyperparameters")
-        trainer = Trainer(params_tuned, run_name=run_name)  # Train model using optimal hyperparameters
+        trainer = Trainer(params_tuned, run_name=run_name, azure=use_azure)
         trainer.train()
 
     print("Start classification")
-    # Test trained models on classification
-    dataset_list = ["Wine", "Rock", "Plane", "ArrowHead", "p2s"]
-    sax_list = [{"n_segments": 128, "alphabet_size": 32}, {"n_segments": 128, "alphabet_size": 48}]
-    n_iters = 10
-
-    classification(dataset_list, run_names, sax_list, n_iters)
+    classification(datasets=cls_datasets, vae_models=run_names, sax_params=sax_params, iters_per_setting=cls_trials)
